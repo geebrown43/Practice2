7121bad26666b6a1d00d4851cdd5f95c804e1535
Gavin Brown <scareb15@gmail.com>
2019-01-10 15:15:17 -0700

    added photos to project

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries : [String] = []
+    var score: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        askQuestion()
     }
-
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal) // setImage assings a UIImage to a button
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 
 }
 
